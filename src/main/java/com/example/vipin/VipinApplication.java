ab67bfad7cc63eeaa95dab846a849428c6d4ec32
9717253548 <suryavanshi2544@gmail.com>
2022-01-14 22:33:45 +0530

    first commit

---
+++ src/main/java/com/example/vipin/VipinApplication.java
@@ -0,0 +1,13 @@
+package com.example.vipin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipinApplication.class, args);
+	}
+
+}
